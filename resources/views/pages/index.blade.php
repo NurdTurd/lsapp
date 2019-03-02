deb4827c32939491f910fb114766703d10eab73f
Anupama-Dikkumbura <anupamadikkubura@gmail.com>
2019-03-02 21:15:59 +0530

    basics

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,13 +1,8 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name','LSAPP')}}</title>
-    </head>
-    <body>
-        <h1>Welcome to laravel</h1>
-        <p>THis is the laravel application from the laravel from scratch youtube seies</p>
-    </body>
-</html>
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>THis is the laravel application from the laravel from scratch youtube seies</p>
+@endsection
+
+
